2522a19ffc87094def52422c7d46afc53cc7dd33
cuongmq <cuongqcmai@gmail.com>
2023-07-03 08:12:02 +0700

    Update package

--- todolist/src/main.jsx
+++ todolist/src/main.jsx
@@ -1,15 +1,41 @@
 import React from "react";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import { TouchBackend } from "react-dnd-touch-backend";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
-import { HTML5Backend } from "react-dnd-html5-backend";
-import { DndProvider } from "react-dnd";
 import { AppProvider } from "./context/AppProvider.jsx";
-import { TouchBackend } from "react-dnd-touch-backend";
+import "./index.css";
+import { MultiBackend } from "react-dnd-multi-backend";
+import {
+  DndProvider,
+  TouchTransition,
+  MouseTransition,
+} from "react-dnd-multi-backend";
+
+export const HTML5toTouch = {
+  backends: [
+    {
+      id: "html5",
+      backend: HTML5Backend,
+      transition: MouseTransition,
+    },
+    {
+      id: "touch",
+      backend: TouchBackend,
+      options: { enableMouseEvents: true },
+      preview: true,
+      transition: TouchTransition,
+    },
+  ],
+};
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <DndProvider backend={HTML5Backend || TouchBackend}>
+    <DndProvider
+      options={HTML5toTouch}
+      backend={MultiBackend}
+      // options={backendOptions}
+    >
       <AppProvider>
         <App />
       </AppProvider>
